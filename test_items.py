c40d4fbe8a95b77ce51606d43073594abe6ccd6a
Yarmak20 <114510685+Yarmak20@users.noreply.github.com>
2022-10-18 13:46:12 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ def test_find_card_button(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     time.sleep(10)
     button = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
-    assert len(button) > 0, 'Кнопка не найдена'
+    assert button
 
 
 

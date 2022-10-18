58f8bd9d3d1c8412e806738fa6e1fd271d01dfd7
Yarmak20 <114510685+Yarmak20@users.noreply.github.com>
2022-10-18 13:43:33 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,20 +1,22 @@
-from selenium import webdriver
 import pytest
-import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose language")
+                     help='Choose language')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    language = request.config.getoption("language")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(10)
-    browser.get(url='http://selenium1py.pythonanywhere.com/' + language + '/catalogue/coders-at-work_207/')
+    user_language = request.config.getoption('language')
+    print("\nstart chrome browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    time.sleep(5)
+    print("\nquit browser..")
     browser.quit()
 
 
